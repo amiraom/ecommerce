d6d9cee5c356b34d66ad406986ac3e862517b5d0
amira <amira.omrani6@gmail.com>
2023-03-22 15:38:50 +0100

    first commit

---
+++ front-end/src/privateRoute/UserRoute.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Navigate,useLocation,Outlet} from 'react-router-dom'
+
+const UserRoute =() => {
+
+    const location = useLocation ();
+    return (
+
+        localStorage.getItem('token') ? <Outlet/> : <Navigate to="/login" state={{from:location} } replace/>
+    )
+}
+export default UserRoute;
